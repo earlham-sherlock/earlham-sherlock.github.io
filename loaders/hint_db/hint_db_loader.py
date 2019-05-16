3c8046dc08a179807aae7b25c63868b17a4526b6
Balazs <bbazsi41@gmail.com>
2019-05-16 14:10:54 +0200

    [Balazs] Some fixes in the HINT Database Loader

--- loaders/hint_db/hint_db_loader.py
+++ loaders/hint_db/hint_db_loader.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 import os
 import json
+import re
+from time import strftime
 
 
 def parse_args(args):
@@ -38,7 +40,13 @@ def parse_args(args):
         
         -int_b_m_tn, --interactor-b-molecule-type-mi-term-name <str>  : MI term name entity type of interactor B [mandatory]
         
-        -int_type_id, --interaction_type_mi_id <int>                  : MI ID of the interaction type [optional]
+        -int_det_m, --interaction-detection-method <int>              : comma separated list of the detection methods of the interaction [optional]
+        
+        -int_type_id, --interaction-type-mi-id <int>                  : comma separated list of MI IDs of the interaction type [optional]
+        
+        -db, --source-db-mi-id <int>                                  : comma separated list of MI IDs of the database sources [optional]
+        
+        -pmid, --pubmed-id <int>                                      : comma separated list of pubmed IDs of the paper [optional]
         
         
         **Exit codes**
@@ -50,6 +58,8 @@ def parse_args(args):
 
         1) The HINT database does not include the mi identifiers of the interaction types!
         2) HINT database does not have any Uniprot Ref identifier, that is why, we give an unique id for it, 10000!
+        3) The interaction type is not in the database file, so we defined it according to the published paper of the database! It is 0915, physical association!
+        4) The pubmed ID of the published paper for the HINT database is 22846459!
         """
 
     parser = argparse.ArgumentParser(description=help_text)
@@ -117,10 +127,31 @@ def parse_args(args):
                         action="store",
                         required=True)
 
-    parser.add_argument("-int_type_id", "--interaction_type_mi_id",
-                        help="<MI ID of the interaction type> [mandatory]",
-                        type=int,
+    parser.add_argument("-int_det_m", "--interaction-detection-method",
+                        help="<comma separated list of the detection methods of the interaction> [optional]",
+                        type=str,
+                        dest="interaction_detection_method",
+                        action="store",
+                        required=False)
+
+    parser.add_argument("-int_type_id", "--interaction-type-mi-id",
+                        help="<comma separated list of MI IDs of the interaction type> [optional]",
+                        type=str,
                         dest="interaction_type_mi_id",
+                        action="store",
+                        required=False)
+
+    parser.add_argument("-db", "--source-db-mi-id",
+                        help="<comma separated list of MI IDs of the database sources> [optional]",
+                        type=str,
+                        dest="source_db_mi_id",
+                        action="store",
+                        required=False)
+
+    parser.add_argument("-pmid", "--pubmed-id",
+                        help="<comma separated list of pubmed IDs of the paper> [optional]",
+                        type=str,
+                        dest="pubmed_id",
                         action="store",
                         required=False)
 
@@ -129,7 +160,8 @@ def parse_args(args):
     return results.input_file, results.interactor_a_id_type.lower(), results.interactor_b_id_type.lower(),\
            results.interactor_a_tax_id, results.interactor_b_tax_id, results.interactor_a_molecule_type_mi_id,\
            results.interactor_a_molecule_type_mi_term_name.lower(), results.interactor_b_molecule_type_mi_id,\
-           results.interactor_b_molecule_type_mi_term_name.lower(), results.interaction_type_mi_id
+           results.interactor_b_molecule_type_mi_term_name.lower(), results.interaction_detection_method,\
+           results.interaction_type_mi_id, results.source_db_mi_id, results.pubmed_id
 
 
 def check_params(input_file):
@@ -141,8 +173,8 @@ def check_params(input_file):
 
 def write_to_output(line, interactor_a_id_type, interactor_b_id_type, interactor_b_tax_id,
                     interactor_a_molecule_type_mi_id, interactor_a_molecule_type_mi_term_name,
-                    interactor_b_molecule_type_mi_id, interactor_b_molecule_type_mi_term_name, interaction_type_mi_id,
-                    out):
+                    interactor_b_molecule_type_mi_id, interactor_b_molecule_type_mi_term_name,
+                    interaction_detection_method, interaction_type_mi_id, source_db_mi_id, pubmed_id, out):
 
     json_dictionary = {}
     json_dictionary["interactor_a_id"] = line[0].lower()
@@ -166,11 +198,45 @@ def write_to_output(line, interactor_a_id_type, interactor_b_id_type, interactor
         detection_method = i.split(":")[1]
         pm_id = i.split(":")[0]
 
-        json_dictionary["interaction_detection_methods_mi_id"].append(int(detection_method))
-        json_dictionary["pmids"].append(pm_id)
+        if detection_method not in json_dictionary["interaction_detection_methods_mi_id"]:
+            json_dictionary["interaction_detection_methods_mi_id"].append(int(detection_method))
+
+        string = re.findall("([A-Za-z])", pm_id)
+
+        if string:
+            continue
+
+        else:
+            if int(pm_id) not in json_dictionary["pmids"]:
+                json_dictionary["pmids"].append(int(pm_id))
+
+    if interaction_detection_method:
+        methods = interaction_detection_method.split(",")
+
+        for method_id in methods:
+            if int(method_id) not in json_dictionary["interaction_detection_methods_mi_id"]:
+                json_dictionary["interaction_detection_methods_mi_id"].append(int(method_id))
 
     if interaction_type_mi_id:
-        json_dictionary["interaction_types_mi_id"].append(interaction_type_mi_id)
+        interaction_types = interaction_type_mi_id.split(",")
+
+        for type_id in interaction_types:
+            if int(type_id) not in json_dictionary["interaction_types_mi_id"]:
+                json_dictionary["interaction_types_mi_id"].append(int(type_id))
+
+    if source_db_mi_id:
+        databases = source_db_mi_id.split(",")
+
+        for db_id in databases:
+            if int(db_id) not in json_dictionary["source_database_mi_id"]:
+                json_dictionary["source_database_mi_id"].append(int(db_id))
+
+    if pubmed_id:
+        pm_ids = pubmed_id.split(",")
+
+        for pmid in pm_ids:
+            if int(pmid) not in json_dictionary["pmids"]:
+                json_dictionary["pmids"].append(int(pmid))
 
     out.write(json.dumps(json_dictionary) + '\n')
 
@@ -179,11 +245,11 @@ def main():
 
     input_file, interactor_a_id_type, interactor_b_id_type, interactor_a_tax_id, interactor_b_tax_id,\
     interactor_a_molecule_type_mi_id, interactor_a_molecule_type_mi_term_name,\
-    interactor_b_molecule_type_mi_id, interactor_b_molecule_type_mi_term_name, interaction_type_mi_id\
-    = parse_args(sys.argv[1:])
+    interactor_b_molecule_type_mi_id, interactor_b_molecule_type_mi_term_name, interaction_detection_method,\
+    interaction_type_mi_id, source_db_mi_id, pubmed_id = parse_args(sys.argv[1:])
 
     check_params(input_file)
-    print(f'MESSSAGE: --- Parameters are fine, starting... ---')
+    print(f'MESSSAGE [{strftime("%H:%M:%S")}]: Parameters are fine, starting...')
 
     output_folder = input_file.split("/")[:-1]
     output_folder = "".join(output_folder)
@@ -197,15 +263,15 @@ def main():
 
         f.readline()
 
-        print(f'MESSSAGE: --- Writing interactions to output file: {output_file} ---')
+        print(f'MESSSAGE [{strftime("%H:%M:%S")}]: Writing interactions to output file: {output_file}')
         for line in f:
             line = line.strip().split('\t')
             write_to_output(line, interactor_a_id_type, interactor_b_id_type, interactor_b_tax_id,
                             interactor_a_molecule_type_mi_id, interactor_a_molecule_type_mi_term_name,
                             interactor_b_molecule_type_mi_id, interactor_b_molecule_type_mi_term_name,
-                            interaction_type_mi_id, out)
+                            interaction_detection_method, interaction_type_mi_id, source_db_mi_id, pubmed_id, out)
 
-    print(f'MESSSAGE: --- HINT Database Loader script finished successfully! ---')
+    print(f'MESSSAGE [{strftime("%H:%M:%S")}]: HINT Database Loader script finished successfully!')
 
 
 if __name__ == '__main__':
--- loaders/hint_db/readme.md
+++ loaders/hint_db/readme.md
@@ -5,8 +5,8 @@
 This script takes a HINT database file, which contains protein-protein
 interactions and converts it to Sherlock compatible JSON format.
 
-The downloaded database file does not contain the parameters below!
-Because of this, the user have to identify these mandatory parameters!
+The downloaded database file does not contain some of the parameters below!
+Because of this, the user have to identify these parameters!
 
 
 **Parameters:**
@@ -29,7 +29,13 @@ Because of this, the user have to identify these mandatory parameters!
 
 -int_b_m_tn, --interactor-b-molecule-type-mi-term-name <str>  : MI term name entity type of interactor B [mandatory]
 
--int_type_id, --interaction_type_mi_id <int>                  : MI ID of the interaction type [optional]
+-int_det_m, --interaction-detection-method <int>              : comma separated list of the detection methods of the interaction [optional]
+
+-int_type_id, --interaction-type-mi-id <int>                  : comma separated list of MI IDs of the interaction type [optional]
+
+-db, --source-db-mi-id <int>                                  : comma separated list of MI IDs of the database sources [optional]
+
+-pmid, --pubmed-id <int>                                      : comma separated list of pubmed IDs of the paper [optional]
 
 
 **Exit codes**
@@ -41,6 +47,9 @@ Exit code 1: The specified input file doesn't exists!
 
 1) The HINT database does not include the mi identifiers of the interaction types!
 2) HINT database does not have any Uniprot Ref identifier, that is why, we give an unique id for it, 10000!
+3) The interaction type is not in the database file, so we defined it according to the published paper of the database!
+It was 0915, physical association!
+4) The pubmed ID of the published paper for the HINT database is 22846459!
 
 
 **Example**
@@ -54,12 +63,12 @@ A0A024QYV7	Q13838	A0A024QYV7	DDX39B	HCG_1994130			HGNC:13917	14667819:0018:HT
 ```
 
 Terminal command:
-python3 hint_db_loader.py -i example_files/HomoSapiens_binary_hq.txt -int_a_id uniprotac -int_b_id uniprotac -int_a_tax_id 9606 -int_b_tax_id 9606 -int_a_m_id 0326 -int_b_m_id 0326 -int_a_m_tn protein -int_b_m_tn protein
+python3 hint_db_loader.py -i example_files/test.tsv -int_a_id uniprotac -int_b_id uniprotac -int_a_tax_id 9606 -int_b_tax_id 9606 -int_a_m_id 0326 -int_b_m_id 0326 -int_a_m_tn protein -int_b_m_tn protein -int_type_id 0915 -pmid 22846459
 
 The output will be:
 - output file: interactor_a_tax_id=9606/hint_db.json
 ```
-{"interactor_a_id": "a0a024qyv7", "interactor_b_id": "a0a024qyv7", "interactor_a_id_type": "uniprotac", "interactor_b_id_type": "uniprotac", "interactor_b_tax_id": 9606, "interactor_a_molecule_type_mi_id": 326, "interactor_b_molecule_type_mi_id": 326, "interactor_a_molecule_type_name": "protein", "interactor_b_molecule_type_name": "protein", "interaction_detection_methods_mi_id": [18], "interaction_types_mi_id": [], "source_database_mi_id": [10000], "pmids": ["14667819"]}
-{"interactor_a_id": "a0a024qyv7", "interactor_b_id": "q01844", "interactor_a_id_type": "uniprotac", "interactor_b_id_type": "uniprotac", "interactor_b_tax_id": 9606, "interactor_a_molecule_type_mi_id": 326, "interactor_b_molecule_type_mi_id": 326, "interactor_a_molecule_type_name": "protein", "interactor_b_molecule_type_name": "protein", "interaction_detection_methods_mi_id": [398, 18], "interaction_types_mi_id": [], "source_database_mi_id": [10000], "pmids": ["16189514", "16189514"]}
-{"interactor_a_id": "a0a024qyv7", "interactor_b_id": "q13838", "interactor_a_id_type": "uniprotac", "interactor_b_id_type": "uniprotac", "interactor_b_tax_id": 9606, "interactor_a_molecule_type_mi_id": 326, "interactor_b_molecule_type_mi_id": 326, "interactor_a_molecule_type_name": "protein", "interactor_b_molecule_type_name": "protein", "interaction_detection_methods_mi_id": [18], "interaction_types_mi_id": [], "source_database_mi_id": [10000], "pmids": ["14667819"]}
+{"interactor_a_id": "a0a024qyv7", "interactor_b_id": "a0a024qyv7", "interactor_a_id_type": "uniprotac", "interactor_b_id_type": "uniprotac", "interactor_b_tax_id": 9606, "interactor_a_molecule_type_mi_id": 326, "interactor_b_molecule_type_mi_id": 326, "interactor_a_molecule_type_name": "protein", "interactor_b_molecule_type_name": "protein", "interaction_detection_methods_mi_id": [18], "interaction_types_mi_id": [915], "source_database_mi_id": [10000], "pmids": [14667819, 22846459]}
+{"interactor_a_id": "a0a024qyv7", "interactor_b_id": "q01844", "interactor_a_id_type": "uniprotac", "interactor_b_id_type": "uniprotac", "interactor_b_tax_id": 9606, "interactor_a_molecule_type_mi_id": 326, "interactor_b_molecule_type_mi_id": 326, "interactor_a_molecule_type_name": "protein", "interactor_b_molecule_type_name": "protein", "interaction_detection_methods_mi_id": [398, 18], "interaction_types_mi_id": [915], "source_database_mi_id": [10000], "pmids": [16189514, 22846459]}
+{"interactor_a_id": "a0a024qyv7", "interactor_b_id": "q13838", "interactor_a_id_type": "uniprotac", "interactor_b_id_type": "uniprotac", "interactor_b_tax_id": 9606, "interactor_a_molecule_type_mi_id": 326, "interactor_b_molecule_type_mi_id": 326, "interactor_a_molecule_type_name": "protein", "interactor_b_molecule_type_name": "protein", "interaction_detection_methods_mi_id": [18], "interaction_types_mi_id": [915], "source_database_mi_id": [10000], "pmids": [14667819, 22846459]}
 ```
