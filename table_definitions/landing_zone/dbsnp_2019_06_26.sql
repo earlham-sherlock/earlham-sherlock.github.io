b1a3b067be400e38f085cd21b29e0402cc6cfaa7
Balazs <bbazsi41@gmail.com>
2019-06-27 15:10:00 +0200

    [Balazs] #SHR-011 Finish with SNP Database Loader script and its table definitions - this is actually the last commit

--- table_definitions/landing_zone/dbsnp_2019_06_26.sql
+++ table_definitions/landing_zone/dbsnp_2019_06_26.sql
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS landing.dbsnp_2019_06_26 (
    partitioned_by    = ARRAY['chr'],
    external_location = 's3a://sherlock/landing_zone/dbsnp_2019_06_26');
 
--- don't forget to refresh the partition list!
+-- don't forget to refresh the partition list in Hive!
